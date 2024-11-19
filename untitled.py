16bc54caed1495b03b5528a66a7c697f7e5dfe15
Thomas <thomaslaene@gmail.com>
2024-11-19 22:14:52 +0100

    Second Commit

--- untitled.py
+++ untitled.py
@@ -1,10 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('Exercise_1_Monthly_Temperature_Data.txt')
 max_temp_index = data['Temperature'].idxmax()
 min_temp_index = data['Temperature'].idxmin()
 
-# Hämta raderna för högsta och lägsta temperatur
 highest_temp = data.loc[max_temp_index]
 lowest_temp = data.loc[min_temp_index]
 
@@ -13,7 +13,22 @@ print("Lägsta temperatur:", lowest_temp)
 
 data['Week'] = (data.index // 7) + 1
 
-# Beräkna medeltemperatur för varje vecka
 weekly_avg = data.groupby('Week')['Temperature'].mean()
 
 print(weekly_avg)
+
+
+plt.figure(figsize=(12, 6))
+plt.plot(data['Temperature'], label='Temperatur över tid', color='blue')
+
+
+plt.scatter(max_temp_index, highest_temp['Temperature'], color='red', label='Högsta temperatur', zorder=5)
+plt.scatter(min_temp_index, lowest_temp['Temperature'], color='green', label='Lägsta temperatur', zorder=5)
+
+
+plt.title('Dagliga temperaturer', fontsize=16)
+plt.xlabel('Dag', fontsize=12)
+plt.ylabel('Temperatur', fontsize=12)
+plt.legend()
+plt.grid()
+plt.show()
